c7ad0dc2f42333ba9e672ae1c40a421583560aa1
heeyame <heeyame@gmail.com>
2023-03-16 14:04:34 +0900

    spring-cloud-stream-kafka-communication - 자바8에 동작하도록 변경 2

--- spring-cloud-stream-kafka-communication/src/main/java/com/amrut/prabhu/dto/Message.java
+++ spring-cloud-stream-kafka-communication/src/main/java/com/amrut/prabhu/dto/Message.java
@@ -3,10 +3,12 @@ package com.amrut.prabhu.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Message {
     public String name;
 }
